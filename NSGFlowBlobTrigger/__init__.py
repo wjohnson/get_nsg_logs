2aaa9ce99745eec3a985ac83ce7fe9e2efa39e39
Will Johnson <wijohns@microsoft.com>
2019-08-12 12:29:58 -0500

    Changed trigger to be based on a queue (implying Event Grid is feeding the queue) and added BlockBlobService to read in the blobs provided by the queue

--- NSGFlowBlobTrigger/__init__.py
+++ NSGFlowBlobTrigger/__init__.py
@@ -1,25 +1,42 @@
 import os, json, socket
 import logging
+import uuid
 
 from .fqdn import FQDNS, get_current_ip_for_fqdns
 from .flowlog import filter_parsed_flowlog, parse_flowlog
+from azure.storage.blob import BlockBlobService
 
 import azure.functions as func
 
 
-def main(nsgblob: func.InputStream, outputblob: func.Out[str]) -> None:
+
+
+def main(msg: func.QueueMessage, outputblob: func.Out[str]) -> None:
+    block_blob_service = BlockBlobService(connection_string = os.environ.get("input_STORAGE"))
+    msg_body = json.loads(msg.get_body().decode('utf-8'))
+    try:
+        blob_path = msg_body["data"]["url"]
+    except:
+        logging.info("Failed to open url for {}".format(msg.id))
+    
     logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name: {nsgblob.name}\n"
-                 f"Blob Size: {nsgblob.length} bytes")
+                 f"Name: {msg.id}")
     
     try:
-        current_nsg = nsgblob.name.split('/')[9]
+        blob_container_name = blob_path.split('/')[3]
+        current_nsg = blob_path.split('/')[8]
+        blob_name = '/'.join(blob_path.split('/')[4:])
     except IndexError:
-        logging.info("Blob found that doesn't have 10 splits.  Exiting.")
+        logging.info("Blob found that doesn't have 9 splits.  Exiting.")
         return None
     # Load in the triggered blob's data
-    data = json.loads(nsgblob.read().decode('utf-8'))
-    # TODO: Filter the list down the last minute
+    localFilename = str(uuid.uuid4())+".json"
+    if os.path.exists(localFilename):
+        os.remove(localFilename)
+    block_blob_service.get_blob_to_path(blob_container_name, blob_name, localFilename)
+    with open(localFilename, 'r', encoding='utf-8') as fp:
+        data = json.load(fp)
+
     # Parse the file to be a list of dictionaries
     parsed_results = parse_flowlog(data, current_nsg)
     logging.info(f"There were {len(parsed_results)} flow log entries found.")
